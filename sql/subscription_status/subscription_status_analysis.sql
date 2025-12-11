7dc14f6b95c429cf821dcd0b5a6f478d967b84d2
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2025-12-11 10:48:48 -0300

    refactor: improve readability

--- sql/subscription_status/subscription_status_analysis.sql
+++ sql/subscription_status/subscription_status_analysis.sql
@@ -3,7 +3,10 @@
 -- Objetivo: Ver como assinantes e não-assinantes se comportam em volume e ticket médio.
 -------------------------------------------------------------
 SELECT
-    Subscription_Status,
+    CASE 
+        WHEN Subscription_Status = 'Yes' THEN 'Assinantes'
+        WHEN Subscription_Status = 'No'  THEN 'Não-assinantes'
+    END AS SubscriptionType,
     COUNT(*) AS TotalUsers,
     ROUND(AVG(Purchase_Amount_USD), 2) AS AverageTicket
 FROM Customer
